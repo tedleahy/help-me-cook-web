0e8858631a70117a14d4cf076c86d1eaafce47e4
Ted Leahy <edwardleahy@protonmail.com>
2020-03-25 17:06:39 +0000

    Toggle add ingredient buttons between '+' and '-' as they're clicked

--- src/components/search-recipes-by-ingredients/SearchIngredientAddRemoveButton.tsx
+++ src/components/search-recipes-by-ingredients/SearchIngredientAddRemoveButton.tsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export default function SearchIngredientAddRemoveBtn() {
+  const [added, setAdded] = useState(false);
+
   return (
-    <Button style={{ fontSize: "x-large" }} className="ml-1 py-0" variant="success">
-      +
+    <Button
+      style={{ fontSize: "x-large", width: "2em" }}
+      className="ml-1 py-0"
+      variant={added ? "danger" : "success"}
+      onClick={() => setAdded(!added)}
+    >
+      {added ? "-" : "+"}
     </Button>
   );
 }
