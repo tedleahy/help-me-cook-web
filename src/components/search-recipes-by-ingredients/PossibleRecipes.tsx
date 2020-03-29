eb8aa0785b895d83e7a8340af12476ee691795c6
Ted Leahy <edwardleahy@protonmail.com>
2020-03-29 12:21:49 +0100

    Retrieve and console.log() all possible recipes the user can cook with their search ingredientson button click

--- src/components/search-recipes-by-ingredients/PossibleRecipes.tsx
+++ src/components/search-recipes-by-ingredients/PossibleRecipes.tsx
@@ -1,10 +1,41 @@
 import React from "react";
 import { Button } from "react-bootstrap";
+import axios, { AxiosResponse, AxiosError } from "axios";
+import { connect } from "react-redux";
+import { AppState } from "../../common/types";
 
-export default function PossibleRecipes() {
+interface PossibleRecipesProps {
+  searchIngredients?: string[];
+}
+
+function PossibleRecipes(props: PossibleRecipesProps) {
+  return <GetPossibleIngredientsBtn searchIngredients={props.searchIngredients} />;
+}
+
+interface GetPossibleIngredientBtnProps {
+  searchIngredients?: string[];
+}
+
+function GetPossibleIngredientsBtn(props: GetPossibleIngredientBtnProps) {
   return (
-    <Button style={{ height: "100%" }}>
+    <Button
+      style={{ height: "100%" }}
+      onClick={() => {
+        axios
+          .post("http://localhost:5000/search_recipes_by_ingredients", {
+            ingredient_names: props.searchIngredients
+          })
+          .then((response: AxiosResponse) => console.log(response.data.data))
+          .catch((error: AxiosError) => console.error(error));
+      }}
+    >
       <b>Get Possible Recipes</b>
     </Button>
   );
 }
+
+const searchIngredientsStore = (appState: AppState) => ({
+  searchIngredients: appState.searchIngredients
+});
+
+export default connect(searchIngredientsStore)(PossibleRecipes);
