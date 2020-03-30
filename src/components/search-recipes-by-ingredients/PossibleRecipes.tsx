d7e9dfe4f4cc3b5e1a177dba3c27857fcff59948
Ted Leahy <edwardleahy@protonmail.com>
2020-03-30 14:31:15 +0100

    Allow user to refresh the list of possible recipes they can make if they change the ingredients they're searching for

--- src/components/search-recipes-by-ingredients/PossibleRecipes.tsx
+++ src/components/search-recipes-by-ingredients/PossibleRecipes.tsx
@@ -1,10 +1,8 @@
-import React, { useState, SetStateAction } from "react";
+import React, { useState } from "react";
 import { Button, ListGroup } from "react-bootstrap";
 import axios, { AxiosResponse, AxiosError } from "axios";
 import { connect } from "react-redux";
 import { AppState, Recipe } from "../../common/types";
-
-type TSetPossibleRecipes = React.Dispatch<React.SetStateAction<Recipe[]>>;
 
 const searchIngredientsStore = (appState: AppState) => ({
   searchIngredients: appState.searchIngredients
@@ -19,54 +17,45 @@ interface PossibleRecipesProps {
 function PossibleRecipes(props: PossibleRecipesProps) {
   const [possibleRecipes, setPossibleRecipes] = useState([] as Recipe[]);
 
-  return possibleRecipes.length ? (
-    <PossibleRecipeList recipes={possibleRecipes} />
-  ) : (
-    <GetPossibleRecipesBtn
-      searchIngredients={props.searchIngredients}
-      setPossibleRecipes={setPossibleRecipes}
-    />
-  );
-}
+  const getPossibleRecipes = () => {
+    axios
+      .post("http://localhost:5000/search_recipes_by_ingredients", {
+        ingredient_names: props.searchIngredients
+      })
+      .then((response: AxiosResponse) => setPossibleRecipes(response?.data?.data))
+      .catch((error: AxiosError) => console.error(error));
+  };
 
-function getPossibleRecipes(
-  searchIngredients = [] as string[],
-  setPossibleRecipes: TSetPossibleRecipes
-) {
-  axios
-    .post("http://localhost:5000/search_recipes_by_ingredients", {
-      ingredient_names: searchIngredients
-    })
-    .then((response: AxiosResponse) => setPossibleRecipes(response?.data?.data))
-    .catch((error: AxiosError) => console.error(error));
+  return (
+    <div className="flex-direction-column w-100 text-center">
+      <GetPossibleRecipesBtn
+        getPossibleRecipes={getPossibleRecipes}
+        possibleRecipesLength={possibleRecipes.length}
+      />
+      <ListGroup style={{ width: "100%", height: "80%" }}>
+        {possibleRecipes.map(recipe => (
+          <ListGroup.Item key={recipe.id}>{recipe.attributes.name}</ListGroup.Item>
+        ))}
+      </ListGroup>
+    </div>
+  );
 }
 
 interface GetPossibleRecipesBtnProps {
-  searchIngredients?: string[];
-  setPossibleRecipes: TSetPossibleRecipes;
+  getPossibleRecipes: () => void;
+  possibleRecipesLength: number;
 }
 
 function GetPossibleRecipesBtn(props: GetPossibleRecipesBtnProps) {
+  const getRecipesBtnClasses = "h-100";
+  const refreshRecipesBtnClasses = "w-100 mb-2";
+
   return (
     <Button
-      className="h-100"
-      onClick={() => getPossibleRecipes(props.searchIngredients, props.setPossibleRecipes)}
+      className={props.possibleRecipesLength ? refreshRecipesBtnClasses : getRecipesBtnClasses}
+      onClick={() => props.getPossibleRecipes()}
     >
-      <b>Get Possible Recipes</b>
+      <b>{props.possibleRecipesLength ? "Refresh" : "Get"} Possible Recipes</b>
     </Button>
-  );
-}
-
-interface PossibleRecipeListProps {
-  recipes: Recipe[];
-}
-
-function PossibleRecipeList(props: PossibleRecipeListProps) {
-  return (
-    <ListGroup style={{ width: "100%", height: "80%" }}>
-      {props.recipes.map(recipe => (
-        <ListGroup.Item key={recipe.id}>{recipe.attributes.name}</ListGroup.Item>
-      ))}
-    </ListGroup>
   );
 }
