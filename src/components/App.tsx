bafce5c593786615ce727ec88360b15b1b9a2cbc
Ted Leahy <edwardleahy@protonmail.com>
2020-04-04 19:22:07 +0100

    Add navbar

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,13 +4,24 @@ import React from "react";
 import { Router } from "@reach/router";
 import CreateShoppingList from "./create-shopping-list/CreateShoppingList";
 import SearchRecipesByIngredients from "./search-recipes-by-ingredients/SearchRecipesByIngredients";
+import { Navbar, Nav } from "react-bootstrap";
 
 export default function App() {
   return (
-    <Router>
-      <SearchRecipesByIngredients path="/" />
-      <CreateShoppingList path="/create-shopping-list" />
-      {/* <SearchRecipesByIngredients path="/search-recipes-by-ingredients" /> */}
-    </Router>
+    <>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="/">HelpMeCook</Navbar.Brand>
+        <Nav>
+          <Nav.Link href="/search-recipes-by-ingredients">Search Recipes</Nav.Link>
+          <Nav.Link href="/create-shopping-list">Create Shopping List</Nav.Link>
+        </Nav>
+      </Navbar>
+
+      <Router>
+        <SearchRecipesByIngredients path="/search-recipes-by-ingredients" />
+        <CreateShoppingList path="/create-shopping-list" />
+        {/* <SearchRecipesByIngredients path="/search-recipes-by-ingredients" /> */}
+      </Router>
+    </>
   );
 }
